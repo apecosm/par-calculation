4eaf7040040fb0326facf34e4d26722545e55125
barriern <nicolas.barrier@ird.fr>
2020-12-07 17:09:13 +0100

    Update error message

--- ncio.cpp
+++ ncio.cpp
@@ -16,7 +16,6 @@
         exit(ERRCODE);                         \
     }
 
-  
 /** Reads a 2D (y, x) output variable. 
  * 
 */
@@ -56,9 +55,9 @@ void read_gridvar(ma3f &var, const char *filename, const char *varname) {
         ERR(status);
     }
 
-    if(mpiRank == 0)  printf("%s: reading %s\n", varname, filename);
+    if (mpiRank == 0)
+        printf("%s: reading %s\n", varname, filename);
 }
-
 
 /** Get the number of time steps stored in a NetCDF output file 
  * 
@@ -72,21 +71,25 @@ size_t get_ntime_file(const char *filename) {
 
     status = nc_open_par(filename, NC_NOWRITE | NC_MPIIO, MPI_COMM_WORLD, MPI_INFO_NULL, &ncid);
     if (status != NC_NOERR) {
+        printf("Error reading %s\n", filename);
         ERR(status);
     }
 
     status = nc_inq_dimid(ncid, TIMEDIMENSION, &dimid);
     if (status != NC_NOERR) {
+        printf("Error inq. %s\n", TIMEDIMENSION);
         ERR(status);
     }
 
     status = nc_inq_dimlen(ncid, dimid, &nsteps);
     if (status != NC_NOERR) {
+        printf("Error inq. dim length %s\n", TIMEDIMENSION);
         ERR(status);
     }
 
     status = nc_close(ncid);
     if (status != NC_NOERR) {
+        printf("Error closing file %s\n", filename);
         ERR(status);
     }
 
@@ -107,11 +110,13 @@ void read_var(ma3f &var, const char *filename, const char *varname, size_t i0, d
 
     status = nc_open_par(filename, NC_NOWRITE | NC_MPIIO, MPI_COMM_WORLD, MPI_INFO_NULL, &ncid);
     if (status != NC_NOERR) {
+        printf("Error reading file %s\n", filename);
         ERR(status);
     }
 
     status = nc_inq_varid(ncid, varname, &varid);
     if (status != NC_NOERR) {
+        printf("Error inq. var %s\n", varname);
         ERR(status);
     }
 
@@ -120,10 +125,13 @@ void read_var(ma3f &var, const char *filename, const char *varname, size_t i0, d
 
     status = nc_get_vara_float(ncid, varid, start, count, var.data());
     if (status != NC_NOERR) {
+        printf("Error reading var %s\n", varname);
         ERR(status);
     }
 
-    if(mpiRank == 0) printf("%s: reading %s, step=%ld\n", varname, filename, i0);
+    if (mpiRank == 0)
+        printf("%s: reading %s, step=%ld\n", varname, filename, i0);
+
     for (size_t k = 0; k < NZ; k++) {
         for (size_t j = 0; j < ny; j++) {
             for (size_t i = 0; i < nx; i++) {
@@ -134,6 +142,7 @@ void read_var(ma3f &var, const char *filename, const char *varname, size_t i0, d
 
     status = nc_close(ncid);
     if (status != NC_NOERR) {
+        printf("Error closing file %s\n", filename);
         ERR(status);
     }
 }
@@ -152,11 +161,13 @@ void read_var(ma2f &var, const char *filename, const char *varname, size_t i0, d
 
     status = nc_open_par(filename, NC_NOWRITE | NC_MPIIO, MPI_COMM_WORLD, MPI_INFO_NULL, &ncid);
     if (status != NC_NOERR) {
+        printf("Error reading file %s\n", filename);
         ERR(status);
     }
 
     status = nc_inq_varid(ncid, varname, &varid);
     if (status != NC_NOERR) {
+        printf("Error inq. variable %s\n", varname);
         ERR(status);
     }
 
@@ -165,6 +176,7 @@ void read_var(ma2f &var, const char *filename, const char *varname, size_t i0, d
 
     status = nc_get_vara_float(ncid, varid, start, count, var.data());
     if (status != NC_NOERR) {
+        printf("Error reading variable %s\n", varname);
         ERR(status);
     }
 
@@ -176,10 +188,12 @@ void read_var(ma2f &var, const char *filename, const char *varname, size_t i0, d
 
     status = nc_close(ncid);
     if (status != NC_NOERR) {
+        printf("Error closing file %s\n", filename);
         ERR(status);
     }
 
-    if(mpiRank == 0)  printf("%s: reading %s, step=%ld\n", varname, filename, i0);
+    if (mpiRank == 0)
+        printf("%s: reading %s, step=%ld\n", varname, filename, i0);
 }
 
 void define_output_file(int cpt) {
@@ -198,26 +212,31 @@ void define_output_file(int cpt) {
 
     status = nc_create_par(ncfile, NC_CLOBBER | NC_NETCDF4 | NC_MPIIO, MPI_COMM_WORLD, MPI_INFO_NULL, &ncid);
     if (status != NC_NOERR) {
+        printf("Error creating filename %s\n", ncfile);
         ERR(status);
     }
 
     status = nc_def_dim(ncid, "time", NC_UNLIMITED, &timeid);
     if (status != NC_NOERR) {
+        printf("Error defining dimension %s\n", "time");
         ERR(status);
     }
 
     status = nc_def_dim(ncid, "z", nz, &zd);
     if (status != NC_NOERR) {
+        printf("Error defining dimension %s\n", "z");
         ERR(status);
     }
 
     status = nc_def_dim(ncid, "y", ny, &yd);
     if (status != NC_NOERR) {
+        printf("Error defining dimension %s\n", "y");
         ERR(status);
     }
 
     status = nc_def_dim(ncid, "x", nx, &xd);
     if (status != NC_NOERR) {
+        printf("Error defining dimension %s\n", "x");
         ERR(status);
     }
 
@@ -226,6 +245,7 @@ void define_output_file(int cpt) {
 
     status = nc_def_var(ncid, output_var, NC_FLOAT, 4, output_ids, &varid);
     if (status != NC_NOERR) {
+        printf("Error defining variable %s\n", output_var);
         ERR(status);
     }
 
@@ -234,16 +254,19 @@ void define_output_file(int cpt) {
 
     status = nc_def_var(ncid, "time", NC_INT, 1, time_ids, &tid);
     if (status != NC_NOERR) {
+        printf("Error defining variable %s\n", "time");
         ERR(status);
     }
 
     status = nc_enddef(ncid); //end definitions: leave define mode
     if (status != NC_NOERR) {
+        printf("Error ending definition file %s\n", ncfile);
         ERR(status);
     }
 
     status = nc_close(ncid);
     if (status != NC_NOERR) {
+        printf("Error closing definition file %s\n", ncfile);
         ERR(status);
     }
 }
@@ -272,16 +295,19 @@ void write_step(int cpt, int step, ma3f var, int time) {
 
     status = nc_inq_varid(ncid, output_var, &varid);
     if (status != NC_NOERR) {
+        printf("Error inq. variable %s\n", output_var);
         ERR(status);
     }
 
     status = nc_var_par_access(ncid, varid, NC_COLLECTIVE);
     if (status != NC_NOERR) {
+        printf("Error setting var access variable %s\n", output_var);
         ERR(status);
     }
 
     status = nc_put_vara_float(ncid, varid, start, count, var.data());
     if (status != NC_NOERR) {
+        printf("Error writting variable %s\n", output_var);
         ERR(status);
     }
 
@@ -291,21 +317,25 @@ void write_step(int cpt, int step, ma3f var, int time) {
 
     status = nc_inq_varid(ncid, "time", &tid);
     if (status != NC_NOERR) {
+        printf("Error inq. variable %s\n", "time");
         ERR(status);
     }
 
     status = nc_var_par_access(ncid, tid, NC_COLLECTIVE);
     if (status != NC_NOERR) {
+        printf("Error setting var access variable %s\n", "time");
         ERR(status);
     }
 
     status = nc_put_vara_int(ncid, tid, tstart, tcount, &time);
     if (status != NC_NOERR) {
+        printf("Error writting var %s\n", "time");
         ERR(status);
     }
 
     status = nc_close(ncid);
     if (status != NC_NOERR) {
+        printf("Error closing var %s\n", ncfile);
         ERR(status);
     }
 }
@@ -324,6 +354,7 @@ void read_parfrac(ma3f &var, const char *filename, const char *varname) {
 
     status = nc_open_par(filename, NC_NOWRITE | NC_MPIIO, MPI_COMM_WORLD, MPI_INFO_NULL, &ncid);
     if (status != NC_NOERR) {
+        printf("Error reading var %s\n", filename);
         ERR(status);
     }
 
@@ -344,9 +375,11 @@ void read_parfrac(ma3f &var, const char *filename, const char *varname) {
 
     status = nc_close(ncid);
     if (status != NC_NOERR) {
+        printf("Error closing file %s\n", "filename");
         ERR(status);
     }
 
-    if(mpiRank == 0)  printf("%s: reading %s, step=[0, %d]\n", varname, filename, NFRAC - 1);
+    if (mpiRank == 0)
+        printf("%s: reading %s, step=[0, %d]\n", varname, filename, NFRAC - 1);
 }
 #endif

145185e093f419b96a72546112746f84f8576057
Nicolas Barrier <nicolas.barrier@ird.fr>
2023-09-22 15:28:06 +0200

    update test files

---
+++ test/test_parameters.cpp
@@ -0,0 +1,53 @@
+
+#include "ncio.h"
+#include "variables.h"
+#include <string.h>
+
+/** Running the test on the ORCA1 grid (Northfold F-pivot, zonal cyclicity) */
+int main(int argc, char *argv[]) {
+
+    read_parameters("parameters.csv");
+
+    if (parameters["time_dimension"].compare("time_counter") != 0) {
+        printf("Error reading time_dimension\n");
+        return 1;
+    }
+
+    if (parameters["parfrac_file"].compare("/home/barrier/Codes/apecosm/git-apecosm-config/gyre/data/clim_GYRE_5d_00010101_00101231_grid_T.nc") != 0) {
+        printf("Error reading parfrac_file\n");
+        return 1;
+    }
+
+    if (parameters["NFRAC"].compare("12") != 0) {
+        printf("Error reading NFRAC\n");
+        return 1;
+    }
+
+    if (parameters["parfrac_var"].compare("soshfldo") != 0) {
+        printf("Error reading parfrac_var\n");
+        return 1;
+    }
+
+    if (parameters["mesh_mask"].compare("/home/barrier/Codes/apecosm/git-apecosm-config/gyre/data/new_mesh_mask.nc") != 0) {
+        printf("Error reading mesh_mask\n");
+        return 1;
+    }
+
+    if (parameters["chl_pattern"].compare("/home/barrier/Codes/apecosm/git-apecosm-config/gyre/data/Chltot.nc") != 0) {
+        printf("Error reading chl_pattern\n");
+        return 1;
+    }
+
+    if (parameters["chl_var"].compare("Chltot") != 0) {
+        printf("Error reading chl_var\n");
+        return 1;
+    }
+
+    if (parameters["conversion_chl"].compare("1.0") != 0) {
+        printf("Error reading conversion_chl\n");
+        return 1;
+    }
+
+    return 0;
+
+}
